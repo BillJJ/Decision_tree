0700f7061976fd39a487de61ed43b10bf55c80c3
Bill <BillJiang2004@hotmail.com>
2022-10-30 15:33:55 -0400

    added random forest finally!

--- example.py
+++ example.py
@@ -13,19 +13,24 @@ from load_datafiles import *
 from classification_tree import ClassificationTree
 from random_forest import ClassificationForest
 
-X, y = load_mobile_price()
+X, y, test, test_index = load_spaceship_titanic()
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
-model = ClassificationForest(n_estimators=10, max_depth=8)
-model.fit(X_train, y_train)
-print(model.score(X_test, y_test))
-print()
-# preds = model.predict(test)
-# preds = preds.tolist()
-# for i in range(len(preds)):
-#     if preds[i]: preds[i] = 'True'
-#     else: preds[i] = 'False'
-#
-# output = pd.DataFrame({'PassengerId':test_index, 'Transported':preds})
-# output.to_csv('datafiles/spaceship-titanic/submission.csv',index=False)
+# model = ClassificationTree(min_samples=12,max_depth=20)
+# model.fit(X_train,y_train)
+# print(model.score(X_test, y_test))
+# model.visualize('datafiles/spaceship-titanic/sample_tree.png')
+
+model = ClassificationForest(n_estimators=30, max_depth=12)
+model.fit(X, y)
+# print(model.score(X_test, y_test))
+
+preds = model.predict(test)
+preds = preds.tolist()
+for i in range(len(preds)):
+    if preds[i]: preds[i] = 'True'
+    else: preds[i] = 'False'
+
+output = pd.DataFrame({'PassengerId':test_index, 'Transported':preds})
+output.to_csv('datafiles/spaceship-titanic/submission.csv',index=False)
